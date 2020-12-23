628e49e3797eb5cfe79114fef79e3b9902a0ff87
王洪涛 <824047914@qq.com>
2020-12-23 13:50:09 +0800

    '.'

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import router from '@/router'
 import TypeNav from './components/TypeNav'
 
 Vue.config.productionTip=false;
+
 Vue.component(TypeNav.name,TypeNav)
+
 new Vue({
     el:'#app',
     render:h=>h(App),
