138537cf40265bf59e6da98a2456ead123194046
王洪涛 <824047914@qq.com>
2021-01-04 08:31:44 +0800

    1

--- src/views/AddCartSuccess/index.vue
+++ src/views/AddCartSuccess/index.vue
@@ -13,7 +13,7 @@
           </div>
         </div>
         <div class="right-gocart">
-          <router-link class="sui-btn btn-xlarge" :to="`/detail/:${skuInfo.id}`">查看商品详情</router-link>
+          <router-link class="sui-btn btn-xlarge" :to="`/detail/${skuInfo.id}`">查看商品详情</router-link>
           <router-link to="/shopcart">去购物车结算 > </router-link>
         </div>
       </div>
